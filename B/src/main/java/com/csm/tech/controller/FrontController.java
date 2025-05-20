a79351e56a276e3d73abdca800ec5004eed060b5
ritesh.parida <csmpl@1234>
2025-05-20 12:17:40 +0530

    file commit

---
+++ B/src/main/java/com/csm/tech/controller/FrontController.java
@@ -0,0 +1,12 @@
+package com.csm.tech.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FrontController {
+	@GetMapping("/get")
+	public String getAPI() {
+		return "Welcome B";
+	}
+}
